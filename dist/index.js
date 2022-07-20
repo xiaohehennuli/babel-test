0a9098f3501d0bba40aefa93700f4cadcfc280ec
mac <mac@xxxxxxxxxxxxxxxxxxx.local>
2022-07-20 19:50:03 +0800

    feat:理解core.js和它的产物的作用，优雅降级,按需引入polyfill

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+import _asyncToGenerator from "@babel/runtime-corejs3/helpers/asyncToGenerator";
+import _Promise from "@babel/runtime-corejs3/core-js-stable/promise";
+import _regeneratorRuntime from "@babel/runtime-corejs3/regenerator";
+
+var func = /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
+    return _regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            console.log(12123);
+
+          case 1:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function func() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+_Promise.resolve().finally();
